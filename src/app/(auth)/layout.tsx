b9459d6faae7e98d72fb98bf185ade05d6149446
Sajay Prakash <sajayprakashk@gmail.com>
2024-11-26 14:49:42 +0000

    initialize shadcn ui

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <div className="mb-4 text-center">
+      <div className="mb-4 text-wrap text-center">
         <h1 className="text-4xl font-bold">
           <span className="text-red-500">En</span>Convo
         </h1>
